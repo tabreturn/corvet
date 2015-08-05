a497be1a216fd388e4bd3dbd2de0fe3b37228c2c
tristan <tristan@x230>
2015-08-05 15:56:42 +0200

    - refine scaffold

--- js/index.js
+++ js/index.js
@@ -1,14 +1,11 @@
 var app = angular.module('corvet', []);
 
 app.controller('svgUpload', function($scope) {
-  $scope.svgImage = 'hello';
   $scope.submit = function() {
     var file = document.getElementById('upload').files[0];
     var reader = new FileReader();
     reader.onload = function() {
-      
       document.getElementById('result').innerHTML = reader.result;
-      
       console.log(file);
       console.log("name : " + file.name);
       console.log("size : " + file.size);
@@ -16,9 +13,5 @@ app.controller('svgUpload', function($scope) {
       console.log("date : " + file.lastModified);
     }
     reader.readAsText(file);
-    
-    var svg = document.getElementById('result');
-    console.log(svg);
-    
   }
 });
