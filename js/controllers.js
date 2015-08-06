4e2a3df5201b757f357ba9c66d527335b3f58dbb
tabreturn <tristan@x230>
2015-08-06 18:19:20 +0200

    - add shape arrays (for rects/circles/paths) to libcorvet - expand debug info to include new libcorvet functions - update drawing to add more test shapes

--- js/controllers.js
+++ js/controllers.js
@@ -4,16 +4,21 @@ corvet.controller('svgUpload', function($scope, messages, CONFIG) {
   $scope.submit = function() {
     var file = document.querySelector('#upload').files[0];
     var submission = '#submission';
-    var assess = new libcorvet(submission + ' svg');
     
     var reader = new FileReader();
     reader.onload = function() {
       document.querySelector(submission).innerHTML = reader.result;
       
+      var assess = new libcorvet(submission + ' svg');
+      assess.getShapes();
+      
       if(CONFIG.DEBUG) {
-        console.log('rects:' + assess.countRects());
-        console.log('circles:' + assess.countCircles());
-        console.log('paths:' + assess.countPaths());
+        console.log('rects(' + assess.countRects()+'):');
+        console.log(assess.rects);
+        console.log('circles(' + assess.countCircles()+'):');
+        console.log(assess.circles);
+        console.log('paths(' + assess.countPaths()+'):');
+        console.log(assess.paths);
         console.log("file name: " + file.name);
         console.log("file size: " + file.size);
         console.log("file type: " + file.type);
--- js/libcorvet.js
+++ js/libcorvet.js
@@ -4,6 +4,10 @@ var libcorvet = function(svg) {
   
   this.svg = svg;
   
+  this.rects = [];
+  this.circles = [];
+  this.paths = [];
+  
   this.countRects = function() {
     return(document.querySelectorAll(this.svg + ' rect').length);
   }
@@ -14,6 +18,44 @@ var libcorvet = function(svg) {
   
   this.countPaths = function() {
     return(document.querySelectorAll(this.svg + ' path').length);
+  }
+  
+  this.shapeArray = function(total, shape, array) {
+    for(var i=0; i<total; i++) {
+      var add = this.shapeAttributes(document.querySelectorAll(this.svg + ' ' +shape)[i]);
+      array[i] = add;
+    }
+  }
+  
+  this.getShapes = function() {
+    this.shapeArray(this.countRects(), 'rect', this.rects);
+    this.shapeArray(this.countCircles(), 'circle', this.circles);
+    this.shapeArray(this.countPaths(), 'path', this.paths);
+  }
+  
+  this.shapeAttributes = function(shape) {
+    var attr = {};
+    // rect attributes
+    attr.width            = shape.getAttribute('width');
+    attr.height           = shape.getAttribute('height');
+    // circle attributes
+    attr.cx               = shape.getAttribute('cx');
+    attr.cy               = shape.getAttribute('cy');
+    attr.r                = shape.getAttribute('r');
+    // path attributes
+    attr.d                = shape.getAttribute('d');
+    // generic attributes
+    attr.transform        = shape.getAttribute('transform');
+    attr.fill             = shape.style.fill;
+    attr.fillopacity      = shape.style.fillOpacity;
+    attr.stroke           = shape.style.stroke;
+    attr.strokeopacity    = shape.style.strokeOpacity;
+    attr.strokewidth      = shape.style.strokeWidth;
+    attr.strokelinecap    = shape.style.strokeLinecap;
+    attr.strokelinejoin   = shape.style.strokeLinejoin;
+    attr.strokemiterlimit = shape.style.strokeMiterlimit;
+    attr.strokedasharray  = shape.style.strokeDasharray;
+    return attr;
   }
   
 }
