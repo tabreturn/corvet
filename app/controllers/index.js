a8bd8893c08f68991af524e1e4fb18eafb9d68ee
tabreturn <git@tabreturn.com>
2016-05-07 15:36:52 +1200

    - wip

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,19 +1,48 @@
+// app/controllers/index.js
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  isDisabled: true,
-
+  headerMessage: 'Coming Soon',
+  responseMessage: '',
   emailAddress: '',
 
-  actualEmailAddress: Ember.computed('emailAddress', function() {
-    console.log('actualEmailAddress function is called: ', this.get('emailAddress'));
-  }),
+  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: Ember.computed.not('isValid'),
 
-  emailAddressChanged: Ember.observer('emailAddress', function() {
-    console.log('observer is called', this.get('emailAddress'));
-  })
+  actions: {
 
-});
+    saveInvitation() {
+      const email = this.get('emailAddress');
+
+      const newInvitation = this.store.createRecord('invitation', { email: email });
+      newInvitation.save();
+
+      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+      this.set('emailAddress', '');
+    }
+  }
+}); 
+
+
 
 var assess = new libcorvet('submission' + ' svg');
+
+
+
+/*
+export default EmberUploader.FileField.extend({
+
+  filesDidChange: function(files) {
+    const uploader = EmberUploader.Uploader.create({
+      url: this.get('url')
+    });
+ 
+    if (!Ember.isEmpty(files)) {
+      // this second argument is optional and can to be sent as extra data with the upload 
+      uploader.upload(files[0], { whatheverObject });
+    }
+  }
+
+});
+*/
