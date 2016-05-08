d5e611f077ad98631e234fa8f682d1e99a80eff6
tabreturn <git@tabreturn.com>
2016-05-08 20:55:22 +1200

    - wip

--- public/corvet.py
+++ public/corvet.py
@@ -6,29 +6,10 @@ import random
 
 conn = sqlite3.connect('results.sqlite')
 c = conn.cursor()
-c.execute("INSERT INTO results (name, score) VALUES ('{}', 20)".format(random.random()))
+t = (str(random.random()),)
+c.execute("INSERT INTO results (name, score) VALUES (?, 20)", t)
 conn.commit()
 conn.close()
-
-'''
-# Never do this -- insecure!
-symbol = 'RHAT'
-c.execute("SELECT * FROM stocks WHERE symbol = '%s'" % symbol)
-
-# Do this instead
-t = ('RHAT',)
-c.execute('SELECT * FROM stocks WHERE symbol=?', t)
-print c.fetchone()
-
-# Larger example that inserts many records at a time
-purchases = [('2006-03-28', 'BUY', 'IBM', 1000, 45.00),
-             ('2006-04-05', 'BUY', 'MSFT', 1000, 72.00),
-             ('2006-04-06', 'SELL', 'IBM', 500, 53.00),
-            ]
-c.executemany('INSERT INTO stocks VALUES (?,?,?,?,?)', purchases)
-'''
-
-
 
 class Corvet(object):
   
@@ -39,11 +20,6 @@ class Corvet(object):
   @cherrypy.expose
   def result(self, length):
     return length
-
-
-print 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-abspath = os.path.dirname(os.path.realpath(__file__))
-
 
 cherrypy.config.update({
   "tools.staticdir.on": True,
