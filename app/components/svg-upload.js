04025afbbe4714ad5858bf9b700f9c2091543fdb
tabreturn <git@tabreturn.com>
2016-05-26 13:14:05 +1200

    - add file uploader/reader

--- app/components/svg-upload.js
+++ app/components/svg-upload.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
+export default Ember.TextField.extend({
+  tagName: 'input',
+  type: 'file',
+  
+  change: function(e) {
+    var file = e.target.files[0];
+    var reader = new FileReader();
+    
+    reader.onload = function(e) {
+      console.log(e.target.result);
+      document.querySelector('#submission').innerHTML = reader.result;
+    };
+    reader.readAsText(file);
+  }
 });
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -12,6 +12,23 @@ export default Ember.Route.extend({
 
 export default Ember.Controller.extend({
   
+  isDisabled: true,
+
+  emailAddressChanged: Ember.observer('emailAddress', function() {
+    console.log(this.target);
+    console.log(this.get('emailAddress'));
+    
+    //var file = document.querySelector('#upload').files[0];
+    //var submission = '#submission';
+    console.log(document.querySelector('#upload').files[0]);
+    var reader = new FileReader();
+    reader.onload = function() {
+      console.log('loaded');
+    };
+    
+    this.set('isDisabled', false);
+  }),
+  
   actions: {
     
     postResult() {
@@ -22,12 +39,53 @@ export default Ember.Controller.extend({
         .done(function( data ) {
           alert( "Data Loaded: " + data );
         });
-    }
+    }    
   }
   
 });
 
 /*
-var assess = new Libcorvet('submission' + ' svg');
-assess.getShapes();
+
+  $scope.submit = function() {
+    var file = document.querySelector('#upload').files[0];
+    var submission = '#submission';
+    
+    var reader = new FileReader();
+    reader.onload = function() {
+      document.querySelector(submission).innerHTML = reader.result;
+      
+      var assess = new libcorvet(submission + ' svg');
+      assess.getShapes();
+      
+      if (CONFIG.DEBUG) {
+        
+        //console.log('circles(' + assess.countShapes('circle') + '):');
+        //console.log(assess.circles);
+        //console.log('ellipse(' + assess.countShapes('ellipse') + '):');
+        //console.log(assess.ellipses);
+        console.log('paths(' + assess.countShapes('path') + '):');
+        console.log(assess.paths);
+        //console.log('rects(' + assess.countShapes('rect') + '):');
+        //console.log(assess.rects);
+        
+        assess.findCorners();
+        
+        //console.log('file name: ' + file.name);
+        //console.log('file size: ' + file.size);
+        //console.log('file type: ' + file.type);
+        //console.log('file date: ' + file.lastModified);
+        
+        //console.log('compare first two circles:');
+        //console.log(assess.compareShape(assess.circles[0], assess.circles[1]));
+        //console.log('compare first two rects:');
+        //console.log(assess.compareShape(assess.rects[0], assess.rects[1]));
+      }
+    }
+    reader.readAsText(file);
+  }
+
+
+corvet.controller('messageDisplay', function($scope, messages) {
+  console.log(messages.test);
+});
 */
